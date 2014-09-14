2bf882ff3dd449ae4ca7064a33a3239f0405c74f
Shusaku FUKUMINE <shumach217@gmail.com>
2014-09-14 11:57:12 +0900

    step1-1

---
+++ IE2014/IE2014/RootViewController.swift
@@ -0,0 +1,36 @@
+//
+//  RootViewController.swift
+//  IE2014
+//
+//  Created by shusaku FUKUMINE on 2014/09/14.
+//  Copyright (c) 2014年 shusaku FUKUMINE. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = UIColor.blueColor()
+    }
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ IE2014/IE2014Tests/IE2014Tests.swift
@@ -0,0 +1,36 @@
+//
+//  IE2014Tests.swift
+//  IE2014Tests
+//
+//  Created by shusaku FUKUMINE on 2014/09/14.
+//  Copyright (c) 2014年 shusaku FUKUMINE. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class IE2014Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
