a729ea8dd83d2d885c502ac98f8c665a88f7b9d6
Shusaku FUKUMINE <shumach217@gmail.com>
2014-09-19 07:42:10 +0900

    step2-1

--- IE2014/IE2014/RootViewController.swift
+++ IE2014/IE2014/RootViewController.swift
@@ -7,11 +7,10 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class RootViewController: UIViewController {
-
-
-    var label : UILabel?
 
 
     override func viewDidLoad() {
@@ -19,25 +18,6 @@ class RootViewController: UIViewController {
 
         self.view.backgroundColor = UIColor.whiteColor()
 
-        var button1 = UIButton(frame: CGRectMake(100, 100, 120, 50))
-        button1.backgroundColor = UIColor.lightGrayColor()
-        button1.setTitle("Button1", forState: .Normal)
-        button1.addTarget(self, action: "pushedButton1:", forControlEvents: .TouchUpInside)
-
-        self.view.addSubview(button1)
-
-        var button2 = UIButton(frame: CGRectMake(100, 170, 120, 50))
-        button2.backgroundColor = UIColor.lightGrayColor()
-        button2.setTitle("Button2", forState: .Normal)
-        button2.addTarget(self, action: "pushedButton2:", forControlEvents: .TouchUpInside)
-
-        self.view.addSubview(button2)
-
-        self.label = UILabel(frame: CGRectMake(50, 350, 220, 80))
-        self.label!.textAlignment = .Center
-        self.label!.text = "Hello, World"
-
-        self.view.addSubview(self.label!)
     }
 
 
@@ -45,14 +25,5 @@ class RootViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-     
 
-    func pushedButton1(sender: UIButton) {
-        self.label!.text = "Button1"
-    }
-
-
-    func pushedButton2(sender: UIButton) {
-        self.label!.text = "Button2"
-    }
 }
