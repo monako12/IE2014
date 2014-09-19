87ecb2dd4ce0cb5439527c41ce276a373e907700
Shusaku FUKUMINE <shumach217@gmail.com>
2014-09-19 09:55:22 +0900

    step2-2

--- IE2014/IE2014/RootViewController.swift
+++ IE2014/IE2014/RootViewController.swift
@@ -13,11 +13,15 @@ import AVFoundation
 class RootViewController: UIViewController {
 
 
+    var playButton : UIButton?
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.view.backgroundColor = UIColor.whiteColor()
 
+        configurePlayButton()
     }
 
 
@@ -25,5 +29,37 @@ class RootViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+
+    func configurePlayButton() {
+        self.playButton = UIButton.buttonWithType(UIButtonType.Custom) as? UIButton
+        self.playButton!.frame = CGRectMake(100, 330, 120, 50)
+        self.playButton!.setTitle("", forState: .Normal)
+
+        self.playButton!.setImage(UIImage(named: "play.png"), forState: .Normal)
+
+        self.playButton!.accessibilityLabel = ""
+        self.playButton!.addTarget(self, action: "pushedPlayButton:", forControlEvents: .TouchUpInside)
+
+        self.view.addSubview(self.playButton!)
+    }
+
+
+    func pushedPlayButton(sender: UIButton) {
+
+        var title : String = "炎のファイター　アントニオ猪木"
+        var path : String = NSBundle.mainBundle().pathForResource(title, ofType: "mp3")!
+        var url : NSURL = NSURL.fileURLWithPath(path)!
+        var error : NSError?
+        var audioPlayer = AVAudioPlayer(contentsOfURL: url, error: &error)
+
+        if (error != nil) {
+            return
+        }
+
+        audioPlayer.volume = 0.5
+        audioPlayer.play()
+    }
+
 
 }
